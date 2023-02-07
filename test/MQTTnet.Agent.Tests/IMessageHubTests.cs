ec4f8e6f90f361a1c8181f4c0eb177c9d75a6f78
zwq00000 <zwq00000@hotmail.com>
2023-02-07 14:25:31 +0800

    fix unit test

---
+++ test/MQTTnet.Agent.Tests/IMessageHubTests.cs
@@ -0,0 +1,47 @@
+namespace MQTTnet.Agent.Tests;
+
+public class IMessageHubTests {
+    private readonly ITestOutputHelper output;
+    private readonly TestFactory factory;
+
+    public IMessageHubTests(ITestOutputHelper outputHelper) {
+        this.output = outputHelper;
+        this.factory = new TestFactory();
+    }
+
+
+    [Fact]
+    public async void TestDispose() {
+        var agent = factory.GetService<IMessageHub>();
+        Assert.NotNull(agent);
+        var subs = await agent.SubscribeAsync<string>("topic");
+        int count = 0;
+        subs.Subscribe(s => {
+            output.WriteLine(s.Payload);
+            count++;
+        });
+        for (var i = 0; i < 10; i++) {
+            await agent.PublishAsync<string>("topic", i.ToString());
+        }
+        await Task.Delay(100);
+        agent.Dispose();
+
+        Assert.Equal(10, count);
+    }
+    [Fact]
+    public async void TestSubscribe() {
+        var agent = factory.GetService<IMessageHub>();
+        Assert.NotNull(agent);
+        int count = 0;
+        using var subs = await agent.SubscribeAsync<string>("topic", s => {
+            output.WriteLine(s.Payload);
+            count++;
+        });
+        for (var i = 0; i < 10; i++) {
+            await agent.PublishAsync<string>("topic", i.ToString());
+        }
+        await Task.Delay(100);
+        Assert.Equal(10, count);
+    }
+
+}
